4def9767d744ae01d14c27dd3a98df113286982b
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 18:14:09 +0200

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab1ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //citim doua numere si verificam daca x este divizibil cu y
+            int x, y;
+            Console.WriteLine("Introduceti doua numere ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (y == 0) Console.WriteLine("indivizibil");
+            else if (x % y == 0) Console.WriteLine("divizibil");
+            else Console.WriteLine("indivizibil");
+        }
+    }
+}
